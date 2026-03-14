14aa957d0cf75d39240f07a128560919069ab8fb
ramsis-hub <77010921+ramsis-hub@users.noreply.github.com>
2026-03-14 11:05:15 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
-#=================================================
-# Description: DIY script
-# Lisence: MIT
-# Author: eSirPlayground
-# Youtube Channel: https://goo.gl/fvkdwm 
-#=================================================
-#1. Modify default IP
+
+# 1. Set default IP (Change 192.168.1.1 if you need a different gateway)
 sed -i 's/192.168.1.1/192.168.1.1/g' openwrt/package/base-files/files/bin/config_generate
 
-#2. Clear the login password
+# 2. Clear default password
 sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
 
-#3. Replace with JerryKuKu’s Argon
-#rm openwrt/package/lean/luci-theme-argon -rf
+# 3. Rename Hostname
+sed -i 's/OpenWrt/R2S-OpenClash/g' openwrt/package/base-files/files/bin/config_generate
